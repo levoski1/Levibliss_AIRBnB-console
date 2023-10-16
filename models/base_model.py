5adc0e5a4aaa2debd4a9dc75422de3a89155b149
Levi <soromtolevi1@gmail.com>
2023-10-16 12:57:54 +0100

    levi

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,23 @@ class BaseModel():
     class BaseModel that defines all common attributes/methods for other classes
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) != 0:
+            for key,value in kwargs.items():
+                if key == "__class__":
+                    continue
+
+                elif (key == "created_at") or (key == "updated_at"):
+                    self.__dict__[key] = datetime.fromisoformat(value)
+
+                else:
+                    self.__dict__[key] = value
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id} {self.__dict__})"
@@ -22,6 +35,6 @@ class BaseModel():
     def to_dict(self):
         dict_copy = self.__dict__.copy()
         dict_copy['__class__'] = self.__class__.__name__
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
         return dict_copy
